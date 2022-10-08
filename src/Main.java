c6fb1c42828deeac5178d8ba36ce2be1d3f7fe78
Alex <seredin-alex@mail.ru>
2022-10-08 19:00:20 +0300

    Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Урок 8. Задание 1.1");
+        double bankTotal = 0; // вклад в банке
+        int monthSum = 15_000; // ежемесячное пополнение
+        double percent = 1.01;
+
+    }
+}
